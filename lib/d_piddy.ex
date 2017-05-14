4cec410ccf94633484c6486e190e11591f901cb8
Michael Pope <mpope.cr@gmail.com>
2017-05-14 16:29:44 -0700

    Create initial commit

---
+++ lib/d_piddy.ex
@@ -0,0 +1,18 @@
+defmodule D.Piddy do
+  @moduledoc """
+  Documentation for D.Piddy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> D.Piddy.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
