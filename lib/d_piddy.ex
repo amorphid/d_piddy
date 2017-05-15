0bde86ad342581d0058294279276eab4cf0b6849
Michael Pope <mpope.cr@gmail.com>
2017-05-14 19:17:23 -0700

    Delete hello world boilerplate

--- lib/d_piddy.ex
+++ lib/d_piddy.ex
@@ -2,17 +2,4 @@ defmodule D.Piddy do
   @moduledoc """
   Documentation for D.Piddy.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> D.Piddy.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/d_piddy_test.exs
+++ test/d_piddy_test.exs
@@ -1,8 +1,3 @@
 defmodule D.PiddyTest do
   use ExUnit.Case
-  doctest D.Piddy
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
